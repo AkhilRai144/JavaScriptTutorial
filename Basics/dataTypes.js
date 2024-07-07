96233e29e33cbf01bf440b024b5e98c7ec99df65
Akhilesh kumar <110653139+AkhilRai144@users.noreply.github.com>
2024-07-07 15:25:22 +0000

    config

---
+++ Basics/dataTypes.js
@@ -0,0 +1,20 @@
+           /*JavaScript DataTypes*/
+
+/*
+Null => object returnType
+String => string returnType
+BigInt => bigInt returnType
+undefined => undefined returnType
+Boolean => True/False boolean returnType
+Number => number returnTpes
+Symbol => use to store unique value symbol returnType
+ */
+
+let num=50;
+let name="Akhil";
+let salary=null;
+let option=true;
+let a;
+
+console.log(typeof salary);
+
