a6665f5527e06f0583e10215ae3219e651253231
Akhilesh kumar <110653139+AkhilRai144@users.noreply.github.com>
2024-07-07 14:31:06 +0000

    config

---
+++ Basics/variable.js
@@ -0,0 +1,17 @@
+const accountId=100;
+let accountEmail="akhileshkumar19@gmail.com";
+var accountName="Akhilesh";
+let salary;//value initailization not mandatory
+
+//const salary;   //'const' declarations must be initialized
+
+//accountId=10;  //const Varibale values can't change
+accountEmail="akhilesh.com";
+accountName="Akhil";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountName);
+console.log(salary);
+
+console.table([accountId,accountEmail,accountName,salary]);
