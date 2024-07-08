3c3ae245acae373090aa29f870144364def90cd7
Akhilesh kumar <110653139+AkhilRai144@users.noreply.github.com>
2024-07-08 06:08:46 +0000

    config

---
+++ Basics/prefixandpostfix.js
@@ -0,0 +1,21 @@
+let x=2;
+let y=x++;
+
+console.log(x);
+console.log(y);
+
+let o=5;
+let p=++o;
+console.log(o);
+console.log(p);
+
+let a=3;
+let b=--a;
+
+console.log(a);
+console.log(b);
+
+let n=10;
+let m=n--;
+console.log(n);
+console.log(m);
