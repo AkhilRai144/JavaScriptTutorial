630a6549be6cff4ff7b4d922870512bd233549ef
Akhilesh kumar <110653139+AkhilRai144@users.noreply.github.com>
2024-07-08 05:30:03 +0000

    conversion

---
+++ Basics/dataTypes_conversion.js
@@ -0,0 +1,17 @@
+        /*Number to String */
+let num=40;
+let stringEx=String(num);
+console.log(typeof stringEx);
+console.log(typeof(num));
+
+              /* String to Number */
+let score="100";
+let scoreNumber=Number(score);
+console.log(typeof(scoreNumber));
+console.log(typeof(score));
+
+                /* Number to Boolean */
+let a=1;
+let boolenEx=Boolean(a);
+console.log(typeof(boolenEx));
+console.log(boolenEx);
