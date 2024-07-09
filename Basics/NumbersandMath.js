a3542b428baf27d950bbccebc804f614625307a2
Akhilesh kumar <110653139+AkhilRai144@users.noreply.github.com>
2024-07-09 06:09:54 +0000

    config

---
+++ Basics/NumbersandMath.js
@@ -0,0 +1,23 @@
+//Number
+const num=24.589755
+console.log(num.toFixed(2))
+console.log(num.toString())
+console.log(num.toPrecision(2))
+console.log(num.toExponential(3))
+let salary=100000000
+console.log(salary.toLocaleString('en-IN'))
+
+
+//+++++++++++++++++++++++++++++Math+++++++++++++++++++++++++
+
+let a=-3
+console.log(Math.abs(a))
+console.log(Math.max(22,0,5,200))
+console.log(Math.min(11,-1,-3,0))
+console.log(Math.ceil(5.01))
+console.log(Math.pow(3,-2))
+console.log(Math.floor(Math.random()*3))
+let min=10
+let max=20
+console.log(Math.floor(Math.random()*(max-min)+min))
+console.log(Math.round(2.4))
