5a1a5e1789ee399bd43bf03065b79a2eba84ed87
Akhilesh kumar <110653139+AkhilRai144@users.noreply.github.com>
2024-07-10 14:02:36 +0000

    dateandtime

---
+++ Basics/dateandtime.js
@@ -0,0 +1,14 @@
+let date = new Date();
+console.log(date.toString());
+console.log(date.toLocaleString());
+console.log(date.toISOString())
+console.log(date.toJSON())
+console.log(date.getDate())
+console.log(date.toUTCString())
+console.log(date.getTimezoneOffset())
+
+let mydate=new Date("10-7-2024")
+console.log(mydate.toLocaleString())
+
+let newDate=Date.now()
+console.log(newDate)
